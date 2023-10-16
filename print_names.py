0699945a450d61c620a383a6350eb8a42d83e3d7
Diogo <diogo.peralta.goncalves@gmail.com>
2023-10-16 19:32:42 +0100

    first commit

--- print_names.py
+++ print_names.py
@@ -1,12 +1,15 @@
 def initial_print():
-	print('This is a name printer')
+    print('This is a name printer')
 
-def print_goncalo():
-	print('Goncalo')
+
+def print_diogo():
+    print('Diogo')
+
 
 def main():
-	initial_print()
-	print_goncalo()
+    initial_print()
+    print_diogo()
+
 
 if __name__ == '__main__':
-        main()
+    main()
